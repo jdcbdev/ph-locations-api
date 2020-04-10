f733ae22674992fe5beffccabf8c6b4880e06ed0
buonzz <buonzz@gmail.com>
2020-04-10 17:23:47 +0800

    transformer for province

--- app/Transformers/ProvinceTransformer.js
+++ app/Transformers/ProvinceTransformer.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const BumblebeeTransformer = use('Bumblebee/Transformer')
+const {build_api_url} = use('App/Helpers')
 
 /**
  * ProvinceTransformer class
@@ -12,10 +13,12 @@ class ProvinceTransformer extends BumblebeeTransformer {
   /**
    * This method is used to transform the data.
    */
-  transform (model) {
+  transform (model, context) {
     return {
      id: model.province_code,
-     name: model.description
+     name: model.description,
+     region_code: model.region_code,
+     href: build_api_url(context.request, 'provinces/' + model.province_code)
     }
   }
 }
